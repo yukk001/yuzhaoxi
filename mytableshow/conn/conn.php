efa1b4d45b93778a543db832d8a5d29928193ce2
yukk001 <yu10670599@163.com>
2015-05-25 15:14:44 +0800

    CONN TO THE DATABASE

---
+++ mytableshow/conn/conn.php
@@ -0,0 +1,32 @@
+<?php
+
+function getconn()
+{
+    $con = mysql_connect("localhost:3336","root","myoa888");
+    $db = mysql_select_db("TD_OA"); //Ñ¡ÔñÊý¾Ý¿â 
+    if (!$con)
+    {
+        die('Could not connect: ' . mysql_error());
+    }
+    else
+    {
+        return $con;
+    }
+}
+
+function exequery($conn="",$sql="")
+{
+    
+    $cursor = mysql_query($sql,$conn);
+    
+    if(!$cursor)
+    {
+        print_r("sql error:".$sql);
+    }
+    
+    return $cursor;
+    
+}
+
+
+?>
